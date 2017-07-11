f0212635d87c2468b97c5ccb50112e56d605d2e8
yangzhanmei <15809244435@163.com>
2017-07-11 19:21:25 +0800

    dynamic display manufacture and firmware

---
+++ js/displayFirmware.js
@@ -0,0 +1,29 @@
+$(function () {
+    var res = ["a", "b"];
+    var manufacturerList = document.getElementById("manufacturerList");
+    var manufacturerString = "";
+    $.each(res, function (index, val) {
+        manufacturerString += "<div class='manufacturer'>" +
+            "<a href='#' class='manufacturerA'>" + val + "</a>" +
+            "</div>"
+    });
+    manufacturerList.innerHTML = manufacturerString;
+
+    $(".manufacturerA").click(function () {
+
+        return getFirmwareByManufacturer(this.innerHTML);
+    })
+});
+
+function getFirmwareByManufacturer(manufacturer) {
+    var res = [];
+    res.push(manufacturer);
+    var firmwareList = document.getElementById("firmwareList");
+    var firmwareString = "";
+    $.each(res, function (index, val) {
+        firmwareString += " <div class='firmware'>" +
+            "<a href=''>" + val + "</a>" +
+            "</div>"
+    });
+    firmwareList.innerHTML = firmwareString;
+}
