59a581ac071d93b9259b9e2ec74abea747925511
yangzhanmei <15809244435@163.com>
2017-07-13 14:05:12 +0800

    add judge conditions

--- js/addFirmware.js
+++ js/addFirmware.js
@@ -3,6 +3,13 @@ $(function () {
         $(".displaySubmit").slideToggle(900);
     });
     $("#close").click(function () {
-        $(".displaySubmit").slideUp()(1000);
+        $(".displaySubmit").slideUp(900);
+        $("#analysisInput").attr("value","").removeAttr("disabled").css("cursor","");
+    });
+    $(".upload-pic").change(function () {
+        if(this.value!==""){
+            $("#analysisInput").attr("value",this.value);
+            $("#analysisInput").attr("disabled","disabled").css("cursor","not-allowed");
+        }
     })
 });
