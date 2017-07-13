d68b05fd5ec980ea03d675b70caa582f90b31814
yangzhanmei <15809244435@163.com>
2017-07-13 13:25:56 +0800

    toggle the submit block

--- js/addFirmware.js
+++ js/addFirmware.js
@@ -1,3 +1,8 @@
-/**
- * Created by yangzhanmei on 17-7-13.
- */
+$(function () {
+    $("#submit").click(function () {
+        $(".displaySubmit").slideToggle(900);
+    });
+    $("#close").click(function () {
+        $(".displaySubmit").slideUp()(1000);
+    })
+});
