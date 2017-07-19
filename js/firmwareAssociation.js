6fc4479658a0ccc20ca8b4f8def85ca3ebf5c10e
yangzhanmei <15809244435@163.com>
2017-07-19 11:30:32 +0800

    add firmwareAssociation

---
+++ js/firmwareAssociation.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#submit").click(function () {
+        $(".displaySubmit").slideToggle(900);
+    });
+    $("#close").click(function () {
+        $(".displaySubmit").slideUp(900);
+        $("#analysisInput").attr("value","").removeAttr("disabled").css("cursor","");
+    });
+    $(".upload-pic").change(function () {
+        if(this.value!==""){
+            $("#analysisInput").attr("value",this.value);
+            $("#analysisInput").attr("disabled","disabled").css("cursor","not-allowed");
+        }
+    })
+});
